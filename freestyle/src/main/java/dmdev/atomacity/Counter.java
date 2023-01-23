d8faecb7cd1aee58718689260e41d0ebc441d945
Alexander Pishvanov <apishvanov@gmail.com>
2023-01-23 15:37:55 +0200

    dmdec atomacity explanation added

---
+++ freestyle/src/main/java/dmdev/atomacity/Counter.java
@@ -0,0 +1,18 @@
+package dmdev.atomacity;
+
+public class Counter {
+    private int count;
+
+    public void increment(){
+        count++;
+    }
+
+    public void decrement(){
+        count--;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+}
